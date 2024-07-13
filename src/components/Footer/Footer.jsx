bc989c5ad527932fa8940c545d04d08921a1df23
nOngMolZ <redem.mnd@gmail.com>
2024-07-13 16:29:51 +0700

    commit first

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <a
+      href="https://github.com/kerBiy/web-portfolio"
+      className="link footer__link"
+    >
+      Copyright &copy; Anon Kreethawate 2024
+    </a>
+  </footer>
+);
+
+export default Footer;
---
+++ src/data/index.js
@@ -0,0 +1,66 @@
+export const header = {
+  homepage: "https://port.nongmolz.xyz",
+  title: "MOLZ",
+};
+
+export const about = {
+  name: "ANON KREETHAWATE",
+  role: "Software Developer",
+  description:
+    "I’m Anon Kreethawate. I graduated in Computer Science from Chaopraya University and completed the Generation Junior Software Developer Bootcamp. I have a passion for programming technology, always keen to learn new languages, and I excel at using AI tools like ChatGPT.",
+  resume: "#",
+  social: {
+    linkedin: "https://www.linkedin.com/in/anon-k-a1412425b/",
+    github: "https://github.com/nOngMolZ",
+  },
+};
+
+export const projects = [
+  {
+    name: "iOS Apps",
+    description:
+      "An iOS app developer featured on the Apple App Store with several applications available for iPhone and iPad. His portfolio includes games such as Merge Block - Hexa Puzzle Chess - 2 Multiplayers, Swipe and Unroll the Ball, Solitaire New Fun Classic, XO Tic Tac Toe! Jelly Edition, and Glow Hockey Xtreme Neon. These games cover a variety of genres, including puzzles, strategy, and sports. Arnon's apps demonstrate his versatility and creativity in game development.",
+    stack: ["Unity", "C#", "Construct 2","Game"],
+    livePreview: "https://kerBiy.github.io/web-portfolio",
+  },
+  {
+    name: "Voyege",
+    description:
+      'Voyage is a project developed during the Generation Junior Software Developer Bootcamp. It is a comprehensive travel booking system designed to streamline the process of planning and reserving trips. The system includes features such as destination selection, accommodation booking, itinerary management, and payment processing. This project showcases the integration of various technical skills acquired during the bootcamp, including front-end development, back-end development, and database management. Voyage aims to provide users with an efficient and user-friendly platform for all their travel needs',
+    stack: ["Javascript", "React", "Node.js", "MongoDB"],
+    sourceCode: "https://github.com/Atreides-Group-5",
+  },
+  {
+    name: "Web Developer",
+    description:
+      "A freelance web developer with expertise in HTML, Tailwind CSS, JavaScript, PHP, and MySQL. Specializes in creating and developing web systems and games, delivering tailored solutions that meet client needs. Capable of building dynamic websites and interactive applications with a commitment to high-quality and efficient development practices.",
+    stack: ["HTML", "CSS", "Javascript", "PHP", "MySQL"],
+    livePreview: "https://kruearth.com/",
+  },
+];
+
+export const skills = [
+  "HTML",
+  "CSS",
+  "PHP",
+  "MySQL",
+  "JavaScript",
+  "React",
+  "Node.js",
+  "MongoDB",
+  "Express",
+  "ES6",
+  "Git",
+  "Java",
+  "Python",
+  "Docker" ,
+  "AWS Cloud",
+  "Tailwind CSS",
+  "Bootstrap CSS",
+  "Figma",
+  "VS Code",
+];
+
+export const contact = {
+  email: "monton.kt@gmail.com",
+};
